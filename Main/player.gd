91fcf2d7a891fe35b987ffc7d37b3306ed8fc56b
Max Estrella <maxestrella565@gmail.com>
2024-09-09 11:42:54 +0100

    Player Movement Setup

---
+++ Main/player.gd
@@ -0,0 +1,21 @@
+extends CharacterBody2D
+
+const SPEED = 300.0
+const JUMP_VELOCITY = -400.0
+
+# Player Movement
+func _physics_process(delta):
+	
+	var directionx = Input.get_axis("ui_left", "ui_right")
+	if directionx:
+		velocity.x = directionx * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		
+	var directiony = Input.get_axis("ui_up", "ui_down")
+	if directiony:
+		velocity.y = directiony * SPEED
+	else:
+		velocity.y = move_toward(velocity.y, 0, SPEED)
+
+	move_and_slide()
