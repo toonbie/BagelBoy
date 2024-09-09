514ceaa226603c9671c62c122ec4d1de882aaa0d
Max Estrella <maxestrella565@gmail.com>
2024-09-09 12:42:14 +0100

    Enemy and Lava

--- Main/player.gd
+++ Main/player.gd
@@ -1,21 +1,25 @@
 extends CharacterBody2D
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+@export var speed = 300.0
+@export var health = 3
 
 # Player Movement
 func _physics_process(delta):
 	
 	var directionx = Input.get_axis("ui_left", "ui_right")
 	if directionx:
-		velocity.x = directionx * SPEED
+		velocity.x = directionx * speed
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.x = move_toward(velocity.x, 0, speed)
 		
 	var directiony = Input.get_axis("ui_up", "ui_down")
 	if directiony:
-		velocity.y = directiony * SPEED
+		velocity.y = directiony * speed
 	else:
-		velocity.y = move_toward(velocity.y, 0, SPEED)
+		velocity.y = move_toward(velocity.y, 0, speed)
 
 	move_and_slide()
+	
+func takeDMG():
+	health -= 1
+	print(health)
