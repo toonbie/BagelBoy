57509e6cedff598b5ef53f7d82b76e39bb523a32
toonbie <milanburny@gmail.com>
2024-09-11 11:37:36 +0100

    Added PauseMenu

---
+++ Main/main.gd
@@ -0,0 +1,24 @@
+extends Node2D
+
+@onready var pause_menu = $PauseMenu.get_child(0)
+var paused = false
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	if Input.is_action_just_pressed("pause"):
+		pauseMenu()
+
+func pauseMenu():
+	if paused:						
+		pause_menu.hide()
+		Engine.time_scale = 1
+	else:
+		pause_menu.show()
+		Engine.time_scale = 0
+		
+	paused = !paused
--- pause_menu.gd
+++ pause_menu.gd
@@ -1,19 +1,18 @@
 extends Control
 
+
 # Called when the node enters the scene tree for the first time.
-func _ready():
+func _ready() -> void:
 	pass # Replace with function body.
 
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+func _process(delta: float) -> void:
 	pass
 
-func _on_continue_pressed() -> void:
-	playerData.health = 3
-	get_tree().change_scene_to_file("res://Main/main.tscn")
-
-func _on_quit_pressed() -> void:
-	get_tree().quit()
 
 func _on_main_menu_pressed() -> void:
 	get_tree().change_scene_to_file("res://main_menu.tscn")
+
+func _on_quit_pressed() -> void:
+	get_tree().quit()
